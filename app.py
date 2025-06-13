b8759945b551f8a783a80e0a8ded2fe81428a4a7
Saanthanu <mail.saanthanuprasad@gmail.com>
2025-06-13 18:48:50 +0530

    Update app.py
    Fix host binding and use dynamic port for Render

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 import pickle
 import numpy as np
+import os
 
 app = Flask(__name__)
 
@@ -38,4 +39,5 @@ def predict():
 
 
 if __name__ == "__main__":
-    app.run(host-'0.0.0.0', debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
